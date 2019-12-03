1dc28dbaca9f99ea8b1a6aae421e069ce0504b78
qrg <qualle.rhythmus+d@gmail.com>
2019-12-03 15:08:36 +0900

    Excel - Create AMP Pages
    * https://nextjs.org/learn/excel/amp

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,10 @@
-import Layout from '../components/MyLayout.js'
-import Link from 'next/link'
+import { useAmp } from 'next/amp';
 
-function PostLink(props) {
-  return (
-    <li>
-      <Link href={`/p/${props.id}`}>
-        <a>{props.title}</a>
-      </Link>
-    </li>
-  )
-}
+export const config = { amp: 'hybrid' };
 
-export default function() {
+export default function Index(props) {
+  const isAmp = useAmp();
   return (
-    <Layout>
-      <h1>My Blog</h1>
-      <ul>
-        <PostLink id="hello-nextjs" title="Hello Next.js" />
-        <PostLink id="learn-nextjs" title="Learn Next.js is awesome" />
-        <PostLink id="deploy-nextjs" title="Deploy apps with Zeit" />
-      </ul>
-    </Layout>
-  )
+    <p>Welcome to the {isAmp ? 'AMP' : 'normal'} version of the Index page!!</p>
+  );
 }
