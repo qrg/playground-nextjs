b406e60a8a400caf10f9c4839430615bcb7bf129
qrg <qualle.rhythmus+d@gmail.com>
2019-12-04 10:59:20 +0900

    build: create-next-app with-typescript
    * `npx create-next-app --example with-typescript`

--- pages/agent.js
+++
@@ -1,8 +0,0 @@
-const Agent = ({ userAgent }) => <h1>Your user agent is: {userAgent}</h1>;
-
-Agent.getInitialProps = async ({ req }) => {
-  const userAgent = req ? req.headers['user-agent'] : navigator.userAgent;
-  return { userAgent };
-};
-
-export default Agent;
