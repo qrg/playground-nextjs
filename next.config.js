679f7f11d3deac7ad03172ce1f5f6b689dd405dd
qrg <qualle.rhythmus+d@gmail.com>
2019-12-08 16:40:42 +0900

    build: use @next/bundle-analyzer
    * https://github.com/zeit/next.js/tree/master/packages/next-bundle-analyzer
    * https://github.com/zeit/next.js/tree/canary/examples/analyze-bundles

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
-module.exports = {
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true'
+});
+
+const nextConfig = {
   async exportPathMap() {
     return {
       '/': { page: '/' }
     };
   }
 };
+
+module.exports = withBundleAnalyzer(nextConfig);
