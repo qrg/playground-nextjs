0305145c1b60c41b4537dcc3ff15666dbe8fe49a
qrg <qualle.rhythmus+d@gmail.com>
2019-12-11 21:18:31 +0900

    feat: add document template
    * Custom <Document>
      * https://nextjs.org/docs#custom-document

---
+++ src/pages/_document.tsx
@@ -0,0 +1,21 @@
+// _document is only rendered on the server side and not on the client side
+// Event handlers like onClick can't be added to this file
+
+import React from 'react';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class CustomDocument extends Document {
+  render(): JSX.Element {
+    return (
+      <Html lang={'en'}>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default CustomDocument;
