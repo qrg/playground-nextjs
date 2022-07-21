f812aad5234dea6c7c86403285afb321198dd3b7
qrg <qualle.rhythmus+d@gmail.com>
2022-07-22 00:30:36 +0900

    chore(lint): Configure ESlint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,3 +1,80 @@
+const base = {
+  root: true,
+  plugins: [],
+  extends: [
+    'eslint:recommended',
+    'plugin:import/recommended',
+    'plugin:import/warnings',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
+    'airbnb',
+    'next/core-web-vitals'
+  ],
+  rules: {
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'builtin',
+          'external',
+          'parent',
+          'sibling',
+          'index',
+          'object',
+          'type'
+        ],
+        pathGroups: [
+          {
+            pattern: '{react,react-dom/**,react-router-dom}',
+            group: 'builtin',
+            position: 'before'
+          },
+          {
+            pattern: '@src/**',
+            group: 'parent',
+            position: 'before'
+          }
+        ],
+        pathGroupsExcludedImportTypes: ['builtin'],
+        alphabetize: {
+          order: 'asc'
+        },
+        'newlines-between': 'always'
+      }
+    ],
+    'react/function-component-definition': [
+      'error',
+      { namedComponents: 'arrow-function' }
+    ]
+  }
+};
+
+const tsConfig = {
+  files: ['**/*.ts', '**/*.tsx'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json'
+  },
+  plugins: [...base.plugins, '@typescript-eslint'],
+  extends: [
+    ...base.extends,
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'airbnb-typescript'
+  ],
+  rules: {
+    ...base.rules
+  }
+};
+
 module.exports = {
-  extends: ['next/core-web-vitals']
+  ...base,
+  extends: [...base.extends, 'prettier'],
+  overrides: [
+    {
+      ...tsConfig,
+      extends: [...tsConfig.extends, 'prettier']
+    }
+  ]
 };
